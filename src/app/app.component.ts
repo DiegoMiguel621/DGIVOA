7d0ba7d1b461e2417d044cf71bf8eaa423f55f12
DiegoMiguel621 <130955808+DiegoMiguel621@users.noreply.github.com>
2025-02-11 15:22:13 -0600

    intento correccion navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    // Restaurar la última ruta visitada
-    const lastRoute = localStorage.getItem('lastRoute');
-    if (lastRoute) {
-      this.router.navigateByUrl(lastRoute);
-    }
-
-    // Guardar la ruta actual cada vez que cambia
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        localStorage.setItem('lastRoute', event.urlAfterRedirects);
-      }
-    });
-  }
+export class AppComponent {
+  title = 'DGIVOA';
 }
