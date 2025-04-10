0160458a527b24d335eec40f319039beed6caca1
Melany Fuentes <jacquelinefuentes@micorreo.upp.edu.mx>
2025-04-10 15:17:15 -0600

    Conexión a la bd
    se creo la bd de visitas y ya funciona agregar una visita

--- src/app/components/visitas/visitas.component.ts
+++ src/app/components/visitas/visitas.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { VisitasService, Visita } from '../../services/visitas.service';
 
 @Component({
   selector: 'app-visitas',
   templateUrl: './visitas.component.html',
   styleUrl: './visitas.component.css'
 })
-export class VisitasComponent implements OnInit{
+export class VisitasComponent implements OnInit {
   isAsideCollapsed = false;
+  visitas: Visita[] = [];
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private visitasService: VisitasService
+  ) {}
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
@@ -18,9 +23,18 @@ export class VisitasComponent implements OnInit{
         this.isAsideCollapsed = (collapsedFromStorage === 'true');
       }
     }
+
+    this.cargarVisitas();
   }
 
   onAsideToggled(collapsed: boolean): void {
     this.isAsideCollapsed = collapsed;
+  }
+
+  cargarVisitas(): void {
+    this.visitasService.obtenerVisitas().subscribe({
+      next: (data) => this.visitas = data,
+      error: (err) => console.error('Error al obtener visitas:', err)
+    });
   }
 }
---
+++ src/app/services/visitas.service.ts
@@ -0,0 +1,32 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Visita {
+  id: number;
+  fecha: string;
+  tipo_dependencia: string;
+  municipio_nombre?: string;
+  nombre: string;
+  asunto: string;
+  observaciones: string;
+  telefono: string;
+  hora_ingreso: string;
+  hora_salida: string;
+  atendio: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VisitasService {
+  private apiUrl = 'http://localhost:3000/api/visitas';
+
+  constructor(private http: HttpClient) {}
+
+  obtenerVisitas(): Observable<Visita[]> {
+    return this.http.get<Visita[]>(this.apiUrl);
+  }
+}
+
